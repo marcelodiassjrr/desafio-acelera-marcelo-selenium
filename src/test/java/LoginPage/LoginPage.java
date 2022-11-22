dffae0c496f8b7e28815a389298c9f67829f952a
marcelodiassjrr <117953725+marcelodiassjrr@users.noreply.github.com>
2022-11-21 23:59:59 -0300

    unnecessary imports were removed

--- src/test/java/LoginPage/LoginPage.java
+++ src/test/java/LoginPage/LoginPage.java
@@ -1,9 +1,6 @@
 package LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import HomePage.ProductsPage;
 import utils.WebDriverUtil;
 
